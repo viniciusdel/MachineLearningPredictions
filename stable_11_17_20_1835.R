a59906f3bcf6254820616e0804ff0dae1946f884
Hunter Stopford <hunterstopford@Hunters-MBP.tampabay.rr.com>
2020-11-24 21:05:59 -0500

    Cosmetic changes.

--- stable_11_17_20_1835.R
+++ stable_11_17_20_1835.R
@@ -7,59 +7,75 @@
 #    http://shiny.rstudio.com/
 #
 
+# load the shiny package
 library(shiny)
 
-# Define UI for application that draws a histogram
+# Define UI for application
 ui <- fluidPage(
     
-    navbarPage("Sci Comp Projects", id = NULL,
+    # Navigation Bar at the top of the Shiny app
+    navbarPage(title = span("Machine Learning Algorithms in Action", style = "color: #532d8e; font-family: Futura; font-size: 25px"), id = NULL,
                
-    tabPanel("Numbers",
-                h4("Click on plot to start drawing, click again to pause"),
-                sliderInput("mywidth", "width of the pencil", min=1, max=30, step=1, value=10),
-                actionButton("reset", "reset"),
-                actionButton("send", "send"),
-                plotOutput("plot", width = "500px", height = "500px",
-                           hover=hoverOpts(id = "hover", delay = 100, delayType = "throttle", clip = TRUE, nullOutside = TRUE),
-                           click="click")),
-    tabPanel("Testing",
-             actionButton("test", "test"),
+               # tab option 1: Number Recognition
+               # mini-project which accepts user-drawn numbers and displays the number as a text output
+               tabPanel(title = "Number Recognition",
+                        h4("Click on the plot to start drawing, click again to pause"),
+                        sliderInput("mywidth", "width of the pencil", min = 1, max = 30, step = 1, value = 10),
+                        actionButton(inputId = "reset", label = "reset", style = "background-color: #ff0000"),
+                        actionButton(inputId = "send", label = "send", style = "background-color: #00ff00"),
+                        plotOutput("plot", width = "500px", height = "500px",
+                                   hover = hoverOpts(id = "hover", delay = 100, delayType = "throttle", clip = TRUE, nullOutside = TRUE),
+                                   click = "click")),
+               # tab option 2: tbd
+               # mini-project which looks at the data from UFC fighters and predicts who would win a fight based off of previous fights
+               tabPanel(title = "Testing",
+                        actionButton("test", "test")),
+               
+               # tab option 3: About
+               # page which displays information about the app
+               tabPanel("About", renderText(h4(("hello"))))
     )
-                )
 )
 
-# Define server logic required to draw a histogram
+
+# Define server logic required
 server <- function(input, output) {
     
     library(imager)
     im <- load.example('parrots') %>% grayscale
     
-    vals = reactiveValues(x=NULL, y=NULL)
+    vals = reactiveValues(x = NULL, y = NULL)
     draw = reactiveVal(FALSE)
+    
     observeEvent(input$click, handlerExpr = {
         temp <- draw(); draw(!temp)
         if(!draw()) {
             vals$x <- c(vals$x, NA)
             vals$y <- c(vals$y, NA)
         }})
+    
+    # Reset button -> Clear the plot
     observeEvent(input$reset, handlerExpr = {
         vals$x <- NULL; vals$y <- NULL
     })
+    
+    # Send button -> send the user-drawn number to the machine learning algorithm
     observeEvent(input$send, handlerExpr = {
         vals$x <- NULL; vals$y <- NULL
     })
+    
     observeEvent(input$hover, {
         if (draw()) {
             vals$x <- c(vals$x, input$hover$x)
             vals$y <- c(vals$y, input$hover$y)
         }})
     output$plot= renderPlot({
-        plot(x=vals$x, y=vals$y, xlim=c(0, 28), ylim=c(0, 28), ylab="y", xlab="x", type="l", lwd=input$mywidth)
+        plot(x = vals$x, y = vals$y, xlim = c(0, 28), ylim = c(0, 28), ylab = "y", xlab = "x", type = "l", lwd = input$mywidth)
         #cat("X value: ", vals$x, "Y Value: ", vals$y, "\n")
     })}
-    #output$values= renderText({
-       #print("Test string")
-    #})
+#output$values= renderText({
+#print("Test string")
+#})
 
 # Run the application 
 shinyApp(ui = ui, server = server)
