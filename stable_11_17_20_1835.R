0704285d687a8ee690c4721f3a2fd22a3dba391e
Hunter Stopford <hunterstopford@Hunters-MBP.tampabay.rr.com>
2020-11-29 22:41:17 -0500

    - Created the UI for Tab 2 - Laid out the foundation for pulling the values from the inputs - Slight commenting edits

--- stable_11_17_20_1835.R
+++ stable_11_17_20_1835.R
@@ -16,7 +16,8 @@ ui <- fluidPage(
     # Navigation Bar at the top of the Shiny app
     navbarPage(title = span("Machine Learning Algorithms in Action", style = "color: #532d8e; font-family: Futura; font-size: 25px"), id = NULL,
                
-               # tab option 1: Number Recognition
+               # *************************
+               # TAB 1: Number Recognition
                # mini-project which accepts user-drawn numbers and displays the number as a text output
                tabPanel(title = "Number Recognition",
                         h4("Click on the plot to start drawing, click again to pause"),
@@ -24,16 +25,36 @@ ui <- fluidPage(
                         actionButton(inputId = "reset", label = "reset", style = "background-color: #ff0000"),
                         actionButton(inputId = "send", label = "send", style = "background-color: #00ff00"),
                         plotOutput("plot", width = "500px", height = "500px",
-                                   hover = hoverOpts(id = "hover", delay = 100, delayType = "throttle", clip = TRUE, nullOutside = TRUE),
-                                   click = "click")),
-               # tab option 2: Diabetes
-               # mini-project which looks at the 
-               tabPanel(title = "Diabetes",
-                        actionButton("test", "test")),
+                                   hover = hoverOpts(id = "hover", delay = 100, delayType = "throttle", clip = TRUE, nullOutside = TRUE),click = "click")),
                
-               # tab option 3: About
+               # *************************
+               # TAB 2: Personal Medical Insurance Costs
+               # mini-project which looks at the multiple factors that are considered when a person registers for Medical Insurance
+               tabPanel(title = "Personal Medical Insurance Costs",
+                        h4("Please fill out the following fields:"),
+                        numericInput(inputId = "age", label = "Age, years", value = 1, min = 1, max = 101, step = 1, width = "150px"),
+                        selectInput(inputId = "sex", label = "Sex, gender", choices = c("Male", "Female"), width = "150px"),
+                        h5("Height:"),
+                        splitLayout(numericInput(inputId = "feet", label = "ft", value = 5, min = 4, max = 8, step = 1, width = "150px"),
+                                    numericInput(inputId = "inches", label = "in.", value = 11, min = 1, max = 11, step = 1, width = "150px"), cellWidths = "150px"),
+                        numericInput(inputId = "weight", label = "Weight, lbs", value = 155, min = 80, max = 300, step = 1, width = "150px"),
+                        textOutput(outputId = "bmi"),
+                        numericInput(inputId = "children", label = "Children", value = 0, min = 0, max = 10, step = 1, width = "150px"),
+                        radioButtons(inputId = "smoker", label = "Smoker?", choices = c("No" = "no", "Yes" = "yes")),
+                        selectInput(inputId = "region", label = "Region you currently reside in the US", choices = c("northeast", "southeast", "southwest", "northwest"), width = "150px"),
+                        actionButton(inputId = "submit", label = "submit", style = "background-color: #DCDCDC")),
+               
+               # *************************
+               # TAB 3: Diabetes
                # page which displays information about the app
-               tabPanel("About", renderText(h4(("hello"))))
+               tabPanel("Diabetes",
+                        h4("This project...")),
+               
+               # *************************
+               # TAB 4: About
+               # page which displays information about the app
+               tabPanel("About",
+                        h4("hello"))
     )
 )
 
@@ -41,12 +62,15 @@ ui <- fluidPage(
 # Define server logic required
 server <- function(input, output) {
     
+    # *************************
+    # TAB 1
     library(imager)
     im <- load.example('parrots') %>% grayscale
     
     vals = reactiveValues(x = NULL, y = NULL)
     draw = reactiveVal(FALSE)
     
+    # Drawing on the plot
     observeEvent(input$click, handlerExpr = {
         temp <- draw(); draw(!temp)
         if(!draw()) {
@@ -77,10 +101,39 @@ server <- function(input, output) {
             vals$x <- c(vals$x, input$hover$x)
             vals$y <- c(vals$y, input$hover$y)
         }})
-    output$plot= renderPlot({
+    output$plot = renderPlot({
         plot(x = vals$x, y = vals$y, xlim = c(0, 28), ylim = c(0, 28), ylab = "y", xlab = "x", type = "l", lwd = input$mywidth)
         #cat("X value: ", vals$x, "Y Value: ", vals$y, "\n")
-    })}
+    })
+    
+    # *************************
+    # TAB 2
+    # submit button -> send all the inputs to the machine learning algorithm
+    observeEvent(input$submit, handlerExpr = {
+        
+        # calculating and rendering BMI
+        output$bmi <- renderText({ c(input$feet, input$inches, input$weight)
+            
+            # converting BMI from Imperial units to Metric units
+            heightFeetToInches <- input$feet * 12
+            heightTotalInches <- heightFeetToInches + input$inches
+            heightTotalMeters <- heightTotalInches * 2.54 * (1/100)
+            
+            weightKilograms <- input$weight * 0.453592
+            
+            bmiMetric <- weightKilograms / (heightTotalMeters^2)
+            substring(bmiMetric, 1, 4)      # trim the amount of digits to 00.0
+            })
+        # these values will be sent to the machine learning algorithm
+        # output$age
+        # output$sex
+        # output$children
+        # output$smoker
+        # output$region
+    })
+    
+    }
+
 #output$values= renderText({
 #print("Test string")
 #})
